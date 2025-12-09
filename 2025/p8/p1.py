b891d24e5a0e6f8bcbca58e56f101d94a7b159aa
Prakhar Pandey <prkpandey942@gmail.com>
2025-12-09 13:21:12 +0530

    done: p8 part 1
    * seems like there was some issue in the problem explanation or i didn't understood it correctly :D

--- 2025/p8/p1.py
+++ 2025/p8/p1.py
@@ -14,9 +14,11 @@ def get_dist(p1, p2):
     return math.sqrt(sum([(a-b)**2 for a, b in zip(p1, p2)]))
 
 def update_cir(p, q):
+    v1 = cir[p]
+    v2 = cir[q]
     for i in range(len(cir)):
-        if cir[i] == cir[q]:
-            cir[i] = cir[p]
+        if cir[i] == v2:
+            cir[i] = v1
 
 # Sorting the pairs in ascending order according to there distance in n*log(n) time complexity
 pairs = [(i, j) for i in range(M) for j in range(i+1, M)]
@@ -25,9 +27,12 @@ pairs = sorted(pairs, key=lambda p: get_dist(nums[p[0]], nums[p[1]]))
 # Getting the smallest pair which is not already connected and updating the circuits
 i = 0
 for t in range(T):
-    while cir[pairs[i][0]] == cir[pairs[i][1]]:
-        i += 1
+    # This was needed according to the explanation in the question
+    # But apparently, not required :(
+    # while cir[pairs[i][0]] == cir[pairs[i][1]]:
+    #     i += 1
     update_cir(pairs[i][0], pairs[i][1])
+    i += 1
 
 # Getting the lens of different circuits and calculating the product of the length of top `TOP` circuits
 lens_dict = dict()
@@ -36,7 +41,7 @@ for c in cir:
         lens_dict[c] += 1
     else:
         lens_dict[c] = 1
-lens = list(lens_dict.values()).sort(reverse=True)
+lens = sorted(list(lens_dict.values()), reverse=True)
 ans = 1
 for i in range(TOP):
     ans *= lens[i]
