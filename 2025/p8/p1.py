9070e1b46eb84f7d638decdc7b087a7257ac620b
Prakhar Pandey <prkpandey942@gmail.com>
2025-12-09 11:44:45 +0530

    chore: improved the efficiency but answer is incorrect

--- 2025/p8/p1.py
+++ 2025/p8/p1.py
@@ -1,6 +1,6 @@
 import math
 
-T = 1000
+T = 999
 TOP = 3
 
 with open('p8/input.txt', 'r') as file:
@@ -8,48 +8,35 @@ with open('p8/input.txt', 'r') as file:
     nums = [[int(n) for n in l.split(",")] for l in lines]
 
 M = len(nums)
-
 cir = list(range(M))
+
+def get_dist(p1, p2):
+    return math.sqrt(sum([(a-b)**2 for a, b in zip(p1, p2)]))
 
 def update_cir(p, q):
     for i in range(len(cir)):
         if cir[i] == cir[q]:
             cir[i] = cir[p]
 
-def get_dist(p1, p2):
-    return math.sqrt(sum([(a-b)**2 for a, b in zip(p1, p2)]))
+# Sorting the pairs in ascending order according to there distance in n*log(n) time complexity
+pairs = [(i, j) for i in range(M) for j in range(i+1, M)]
+pairs = sorted(pairs, key=lambda p: get_dist(nums[p[0]], nums[p[1]]))
 
-def get_smallest():
-    sv, si, sj = -1, -1, -1
-    for i in range(M):
-        for j in range(i+1, M):
-            if cir[i] != cir[j]:
-                if sv == -1:
-                    sv = get_dist(nums[i], nums[j])
-                    si, sj = i, j
-                else:
-                    v = get_dist(nums[i], nums[j])
-                    if v < sv:
-                        sv, si, sj = v, i, j
-    return si, sj
-
+# Getting the smallest pair which is not already connected and updating the circuits
+i = 0
 for t in range(T):
-    i, j = get_smallest()
-    update_cir(i, j)
+    while cir[pairs[i][0]] == cir[pairs[i][1]]:
+        i += 1
+    update_cir(pairs[i][0], pairs[i][1])
 
-
+# Getting the lens of different circuits and calculating the product of the length of top `TOP` circuits
 lens_dict = dict()
-
 for c in cir:
     if c in lens_dict:
         lens_dict[c] += 1
     else:
         lens_dict[c] = 1
-
-
-lens = [v for v in lens_dict.values()]
-lens.sort(reverse=True)
-
+lens = list(lens_dict.values()).sort(reverse=True)
 ans = 1
 for i in range(TOP):
     ans *= lens[i]
